1e991baebbfa7f8c5b0dc012c4208554c85ff581
suraj548 <surajgudaji548@gmail.com>
2024-07-04 00:44:07 +0530

    test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/suraj548/go-calc
+
+go 1.21.0
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/suraj548/go-calc/add"
+)
+
+func main() {
+	fmt.Println("Hello from main")
+	add.Add()
+}
